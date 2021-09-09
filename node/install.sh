89965d5dfff5c90c3cf5287043535625e1fe11c0
avidan-H <mincrodo@gmail.com>
2021-09-09 11:00:52 +0300

    Test if nvm exists already before installing

--- node/install.sh
+++ node/install.sh
@@ -7,7 +7,9 @@
 # Check for nvm
 if test ! "$(which nvm)"; then
   echo "Installing nvm..."
-  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-  cd "$NVM_DIR"
-  git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)) && \. "$NVM_DIR/nvm.sh"
+  default_nvm_dir="$HOME/.nvm"
+  nvm_dir="${NVM_DIR:-$default_nvm_dir}"
+  git clone https://github.com/nvm-sh/nvm.git "$nvm_dir"
+  cd "$nvm_dir"
+  git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)) && \. "$nvm_dir/nvm.sh"
 fi
