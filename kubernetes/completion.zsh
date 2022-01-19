96ee62949733be4b2f348c6ebe6eacb051ff35af
avidan-H <mincrodo@gmail.com>
2022-01-19 12:49:47 +0200

    fix(completions): fix kubectl completion

--- kubernetes/completion.zsh
+++ kubernetes/completion.zsh
@@ -1,5 +1,14 @@
 
 # enable kubectl completion
-if test "$(which kubectl)"; then
-    source <(kubectl completion zsh)
+# copied from the kubectl oh-my-zsh plugin https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/kubectl/kubectl.plugin.zsh
+if (( $+commands[kubectl] )); then
+  # If the completion file does not exist, generate it and then source it
+  # Otherwise, source it and regenerate in the background
+  if [[ ! -f "$ZSH_CACHE_DIR/completions/_kubectl" ]]; then
+    kubectl completion zsh >| "$ZSH_CACHE_DIR/completions/_kubectl"
+    source "$ZSH_CACHE_DIR/completions/_kubectl"
+  else
+    source "$ZSH_CACHE_DIR/completions/_kubectl"
+    kubectl completion zsh >| "$ZSH_CACHE_DIR/completions/_kubectl" &|
+  fi
 fi
