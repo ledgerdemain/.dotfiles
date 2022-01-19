d2795cd47b830b29fbad7b2ad9e6466c4165a0c2
avidan-H <mincrodo@gmail.com>
2022-01-19 18:12:42 +0200

    feat(python): add pdm to installed python utilities

--- python/completion.zsh
+++ python/completion.zsh
@@ -8,3 +8,18 @@ fi
 if test "$(which pipenv)"; then
     eval "$(_PIPENV_COMPLETE=zsh_source pipenv)"
 fi
+
+# enable pdm completion
+# adapted from the kubectl oh-my-zsh plugin https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/kubectl/kubectl.plugin.zsh
+if (( $+commands[pdm] )); then
+  # If the completion file does not exist, generate it and then source it
+  # Otherwise, source it and regenerate in the background
+  if [[ ! -f "$ZSH_CACHE_DIR/completions/_pdm" ]]; then
+    pdm completion zsh >| "$ZSH_CACHE_DIR/completions/_pdm"
+    autload -U "$ZSH_CACHE_DIR/completions/_pdm"
+  else
+    unfunction _pdm
+    autoload -U "$ZSH_CACHE_DIR/completions/_pdm"
+    pdm completion zsh >| "$ZSH_CACHE_DIR/completions/_pdm" &|
+  fi
+fi
