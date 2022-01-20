b56529bebb7cd28e439844bd29baf8540c708570
avidan-H <mincrodo@gmail.com>
2022-01-20 10:28:48 +0200

    fix(terminal_customization): only add zsh-autosuggestions and zsh-syntax-highlighting to plugins list if installed

--- terminal_customization/env.zsh
+++ terminal_customization/env.zsh
@@ -3,9 +3,14 @@ ZSH_THEME="robbyrussell"
 plugins=(
     git
     macos
-    zsh-syntax-highlighting
-    zsh-autosuggestions
     docker
     docker-compose
 )
+if [[ -e "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting ]]; then
+    plugins=("${plugins[@]}" zsh-syntax-highlighting)
+fi
+if [[ -e "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions ]]; then
+    plugins=("${plugins[@]}" zsh-autosuggestions)
+fi
+
 source $ZSH/oh-my-zsh.sh
