60a9bc9f3cdf1d2ed117e32abd30964f33317d3a
avidan-H <>
2023-02-09 11:50:43 +0200

    feat(utilities): set AWS_PROFILE env var

--- utilities/path.zsh
+++ utilities/path.zsh
@@ -1,2 +1,10 @@
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/google-cloud-sdk/path.zsh.inc"; fi
+
+
+# set default AWS profile if aws cli is installed and 'development' profile is found
+if (( $+commands[aws] )); then
+    if $(aws configure list-profiles | grep -q "development"); then
+        export AWS_PROFILE=development
+    fi
+fi
